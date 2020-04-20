fc72a8a592afff394854f7079f8d4a1449d901f7
murong <duoyi_xie@163.com>
2020-04-20 12:10:00 +0800

    首个功能测试，使用unittest

---
+++ functional_tests.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('TO-DO', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
