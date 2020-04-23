0a4e7ee66424ee0c974336714f5148f285fb8aca
murong <duoyi_xie@163.com>
2020-04-23 20:30:25 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,8 @@ from django.test import TestCase
 # Create your tests here.
 from selenium.webdriver.firefox import webdriver
 
+from lists.models import Item
+
 
 class HomePageTest(TestCase):
 
@@ -16,5 +18,20 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
-    def setUssp(self):
-        self.browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
